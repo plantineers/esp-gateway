38be4223ffaec800e6a042d81b97743ece810ca6
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-06-01 22:42:28 +0200

    Implement Data conversion

--- serial-forwarder/src/main.rs
+++ serial-forwarder/src/main.rs
@@ -10,12 +10,69 @@ pub struct Data {
     value: f32,
 }
 
+/// Since the server doesnt rewrite the request we have to rewrite it into their format here
+/// The format is as follows:
+/// ```json
+/// {
+///    "data": [
+///        {
+///            "controller": "a955f72e-1e90-492f-bc62-a2145dd39f38",
+///            "sensor": "temperature",
+///           "value": 20.7
+///        }
+///    ]
+///}
+/// ```
+#[derive(Debug, Serialize, Deserialize)]
+struct ServerData {
+    data: Vec<ServerDataEntry>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ServerDataEntry {
+    controller: String,
+    sensor: String,
+    value: f32,
+}
+
+impl From<SensorData> for ServerData {
+    fn from(data: SensorData) -> Self {
+        let mut server_data = ServerData {
+            data: Vec::new(),
+        };
+        for sensor in data.sensors {
+            server_data.data.push(ServerDataEntry {
+                /// We have to insert '-' into the UUID
+                controller: format!(
+                    "{:?}-{:?}-{:?}-{:?}-{:?}",
+                    &data.controller[0..8],
+                    &data.controller[8..12],
+                    &data.controller[12..16],
+                    &data.controller[16..20],
+                    &data.controller[20..32]
+                ),
+                sensor: sensor.r#type,
+                value: sensor.value,
+            });
+        }
+        server_data
+    }
+}
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
 }
 
-async fn publish_data(data: SensorData) -> reqwest::Result<()>{
+async fn publish_data(client: &mut reqwest::Client, data: SensorData) -> reqwest::Result<()>{
+    let server_data: ServerData = data.into();
+    let res = client.post(format!("{}sensor-data", env!("ENDPOINT").to_string()))
+        .header("Content-Type", "application/json")
+        .header("Authentication", format!("Basic {}", env!("AUTH").to_string())) 
+        .json(&server_data)
+        .send()
+        .await?;
+    res.error_for_status()?;
     Ok(())
 }
 
@@ -29,5 +86,18 @@ mod tests{
         let data = read_to_string("testdata.json").unwrap();
         let decoded: SensorData = serde_json::from_str(&data).unwrap();
         println!("{:?}", decoded);
+    }
+
+    #[tokio::test]
+    async fn test_publish_data(){
+        let data = SensorData{
+            controller: ['t'; 32],
+            sensors: vec![Data{
+                r#type: "test".to_string(),
+                value: 1.0,
+            }],
+        };
+        let mut client = reqwest::Client::new();
+        let res = publish_data(&mut client, data).await.unwrap();
     }
 }
